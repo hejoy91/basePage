8639ea6983dd8eb8da700f3526eca52cb2d96e76
zhanghj <zhanghaijun@yocaly.com>
2019-03-12 17:52:11 +0800

    修改了布局；

--- res/js/index.js
+++ res/js/index.js
@@ -1,8 +1,31 @@
 layui.use(["layer"]);
-
+var contentOpen = false;
+var bindClick = function(){
+	$(".loging").on("dblclick",function(){
+		if(!contentOpen){
+			contentOpen = true;
+			$(".content-main .zw .loging").css("overflow-y","scroll");
+			//$(".content-main .zw .loging").addClass("overflow-scroll");
+			layer.open({
+				type: 1,
+				content: $(this),
+				title: "回忆是一本苦涩的书",
+				closeBtn: 0,
+				shadeClose: true,
+				shade: 0.6,
+				area:["600px",],
+				end: function(){
+					contentOpen = false;
+					$(".content-main .zw .loging").css("overflow-y","hidden");
+				}
+			})
+		}
+	});
+}
 $(document).ready(function(){
   	$("#txt").load("test.txt");
   	$(".btnTest").on("click",function(){
   		layer.alert("welcome!");
   	})
+  	bindClick();
 });
